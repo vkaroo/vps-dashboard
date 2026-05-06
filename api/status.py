f99c486a1f49a0dc33305de6552564361f91b361
vkaroo <blissos741@gmail.com>
2026-05-07 04:06:41 +0800

    fix: update handler to BaseHTTPRequestHandler format for Vercel Python runtime

--- api/status.py
+++ api/status.py
@@ -1,23 +1,20 @@
+from http.server import BaseHTTPRequestHandler
 import http.client
 import json
-import ssl
 
-def handler(request, response):
-    """Vercel serverless function - proxy to VPS API."""
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET'
-    response.headers['Content-Type'] = 'application/json'
-    
-    try:
-        # Connect to VPS API
-        conn = http.client.HTTPConnection('129.226.213.48', timeout=5)
-        conn.request('GET', '/api/status')
-        res = conn.getresponse()
-        data = res.read().decode()
-        conn.close()
-        
-        response.status_code = 200
-        response.send(data)
-    except Exception as e:
-        response.status_code = 500
-        response.send(json.dumps({'error': str(e)}))
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        try:
+            conn = http.client.HTTPConnection('129.226.213.48', timeout=5)
+            conn.request('GET', '/api/status')
+            res = conn.getresponse()
+            data = res.read().decode()
+            conn.close()
+            self.wfile.write(data.encode())
+        except Exception as e:
+            self.wfile.write(json.dumps({'error': str(e)}).encode())
+        return
